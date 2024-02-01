195560b78a46b913f638d1b112346ed0772f3437
ovchinikov <sergeyvinokov@gmail.com>
2024-02-01 22:16:43 +0300

    morgan edit

--- index.js
+++ index.js
@@ -5,12 +5,8 @@ const cors = require('cors')
 app.use(cors())
 
 const morgan = require('morgan')
-app.use(morgan('tiny'))
 
-morgan.token('body', (req) => {
-  JSON.stringify(req.body)
-})
-
+morgan.token('body', (req, res) => JSON.stringify(req.body))
 app.use(
   morgan(':method :url :status :res[content-length] - :response-time ms :body'),
 )
