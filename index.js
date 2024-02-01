d75b5a7ad7a58f128f9b3bacdbffc871be15315d
ovchinikov <sergeyvinokov@gmail.com>
2024-02-01 21:29:05 +0300

    change morgan syntax

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = require("express")
+const express = require('express')
 const app = express()
 app.use(express.json())
 const cors = require('cors')
@@ -7,95 +7,94 @@ app.use(cors())
 const morgan = require(`morgan`)
 app.use(morgan(`tiny`))
 
-morgan.token(`body`, function (req, res) {
-  return `${JSON.stringify(req.body)}`
+morgan.token(`body`, (req) => {
+  JSON.stringify(req.body)
 })
 
-app.use(morgan(":method :url :status :res[content-length] - :response-time ms :body"))
+app.use(
+  morgan(':method :url :status :res[content-length] - :response-time ms :body'),
+)
 
 let persons = [
-    {
-      name: "Arto Hellas",
-      number: "040-123456",
-      id: 1,
-    },
-    {
-      name: "Ada Lovelace",
-      number: "39-44-5323523",
-      id: 2,
-    },
-    {
-      name: "Dan Abramov",
-      number: "12-43-234345",
-      id: 3,
-    },
-    {
-      name: "Mary Poppendieck",
-      number: "39-23-6423122",
-      id: 4,
-    }
+  {
+    name: 'Arto Hellas',
+    number: '040-123456',
+    id: 1,
+  },
+  {
+    name: 'Ada Lovelace',
+    number: '39-44-5323523',
+    id: 2,
+  },
+  {
+    name: 'Dan Abramov',
+    number: '12-43-234345',
+    id: 3,
+  },
+  {
+    name: 'Mary Poppendieck',
+    number: '39-23-6423122',
+    id: 4,
+  },
 ]
 
-app.get("/api/persons",(req, res) => {
-    res.json(persons)
-}) 
+app.get('/api/persons', (req, res) => {
+  res.json(persons)
+})
 
-app.get("/info",(req, res,) => {
+app.get('/info', (req, res) => {
   const currentDate = new Date()
-  res.send(`<h2>Phonebook has info for ${persons.length} people</h2> <h2>${currentDate}</h2>`)
-}) 
+  res.send(
+    `<h2>Phonebook has info for ${persons.length} people</h2> <h2>${currentDate}</h2>`,
+  )
+})
 
-app.get("/api/persons/:id",(req, res) => {
+app.get('/api/persons/:id', (req, res) => {
   const id = req.params.id
-  const entry = persons.find(entry => entry. id == id)
+  const entry = persons.find((entry) => entry.id == id)
 
-  if(entry){
-      res.json(entry)
+  if (entry) {
+    res.json(entry)
   } else {
-      res.status(404).end()   
+    res.status(404).end()
   }
-}) 
+})
 
-app.delete("/api/persons/:id",(req, res) => {
+app.delete('/api/persons/:id', (req, res) => {
   const id = Number(req.params.id)
-  persons = persons.filter(entry => entry.id != id)
-  res.status(204).end();
+  persons = persons.filter((entry) => entry.id != id)
+  res.status(204).end()
 })
 
 const generateId = () => {
-  const maxId = persons.length > 0
-    ? Math.max(...persons.map(n => n.id))
-    : 0
+  const maxId = persons.length > 0 ? Math.max(...persons.map((n) => n.id)) : 0
   return maxId + 1
 }
 
-app.post("/api/persons", (req,res) => {
+app.post('/api/persons', (req, res) => {
   const body = req.body
 
-  if(!body.name){
-    return res.status(400).json({eror: "name is missing"})
+  if (!body.name) {
+    return res.status(400).json({ eror: 'name is missing' })
   }
 
-  if(!body.number){
-    return res.status(400).json({eror: "number is missing"})
+  if (!body.number) {
+    return res.status(400).json({ eror: 'number is missing' })
   }
 
-  if(persons.some(entry => entry.name === body.name)){
-    return res.status(409).json({eror: "name must be unique"})
+  if (persons.some((entry) => entry.name === body.name)) {
+    return res.status(409).json({ eror: 'name must be unique' })
   }
-
 
   let entry = {
     name: body.name,
     number: body.number,
-    id: generateId()
+    id: generateId(),
   }
-  
+
   persons.push(entry)
   res.json(entry)
 })
-
-
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
